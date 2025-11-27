e9c10663384eb05792a2e2d5810b7d7d793050fe
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-11-27 23:13:21 +0800

    :heavy_plus_sign: Add platformdirs

--- lua/git2/main.lua
+++ lua/git2/main.lua
@@ -1,55 +1,11 @@
 ---core functions
 local git2 = require "git2"
-local fs = require "git2.fs"
+local fs = require "platformdirs.fs"
+local fn = require "platformdirs.fn"
 local get_parser = require "git2.argparse".get_parser
 local data = require 'git2.data'.get()
 
 local M = {}
-
----walk all directories except `.git/`
----@param idx table
----@param path string
----@param git_dir string
----@param callback function
-function M.walk(idx, path, git_dir, callback)
-    if fs.isdirectory(path) then
-        for file in fs.dir(path) do
-            if file ~= "." and file ~= ".." and file ~= ".git" then
-                M.walk(idx, fs.joinpath(path, file), git_dir, callback)
-            end
-        end
-    else
-        local git_path = fs.relpath(git_dir, path)
-        callback(idx, git_path)
-    end
-end
-
----process files from index to handle directory
----@param idx table
----@param file string
----@param callback function
-function M.process_files_from_index(idx, file, callback)
-    if idx:get_bypath(file, 0) then
-        callback(idx, file)
-    end
-    for i = 0, idx:entrycount() - 1 do
-        local path = idx:get_byindex(i):path()
-        if path:sub(1, #file + 1) == file .. '/' then
-            callback(idx, path)
-        end
-    end
-end
-
----add file to index
----@param idx table
----@param file string
-function M.add_file_to_index(idx, file)
-    if fs.filereadable(file) then
-        idx:add_bypath(file)
-    else
-        idx:remove(file, 0)
-    end
-end
 
 ---core function
 ---@param args table
@@ -72,7 +28,7 @@ function M.exe(args)
         git2.Repository.init(args.directory, 0)
         return
     end
-    local git_dir = fs.expand(args.C)
+    local git_dir = fn.expand(args.C)
     local repo = git2.Repository.open(git_dir)
     if repo == nil then
         print(args.C .. ' is not a git repository!')
@@ -81,8 +37,6 @@ function M.exe(args)
     local idx = repo:index()
 
     if args.add then
-        -- FIXME: https://github.com/libgit2/luagit2/issues/10
-        -- current realization is slower than `git_index_add_all()`
         if args.A then
             args.file = { git_dir }
         end
@@ -90,13 +44,11 @@ function M.exe(args)
             if file:sub(1, 1) ~= "/" then
                 file = fs.joinpath(fs.getcwd(), file)
             end
-            local git_path = fs.relpath(git_dir, file)
-            M.process_files_from_index(idx, git_path, M.add_file_to_index)
-            M.walk(idx, fs.expand(file), git_dir, idx.add_bypath)
+            idx:add_all(git2.StrArray(file), 0)
         end
     elseif args.rm then
         for _, file in ipairs(args.file) do
-            file = fs.expand(file)
+            file = fn.expand(file)
             idx:remove(file, 0)
             if not args.cached then
                 os.remove(file)
--- template.rockspec
+++ template.rockspec
@@ -16,7 +16,7 @@ description = {
   $license
 }
 
-dependencies = { 'lua >= 5.1', 'argparse', 'mega.cmdparse >= 1.2.1', 'lua-git2', 'luafilesystem' }
+dependencies = { 'lua >= 5.1', 'argparse', 'mega.cmdparse >= 1.2.1', 'lua-git2-tmp', 'luafilesystem', 'platformdirs' }
 
 test_dependencies = $test_dependencies
 
